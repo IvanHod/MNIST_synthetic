08101b6cae71ee4f58e8ddec030bca46908e6220
IvanHod <Khodnenko.Ivan@gmail.com>
2025-08-26 12:27:23 +0300

    change angle argument to rotate argument

--- mnist_synthetic/torch/datasets/mnist_synthetic_dataset_rotated.py
+++ mnist_synthetic/torch/datasets/mnist_synthetic_dataset_rotated.py
@@ -1,59 +1,78 @@
-from typing import Any, Optional, Callable, Literal
+from typing import Any, Optional, Callable
 from pathlib import Path
 
-import cv2
 import numpy as np
+import torch
 from PIL import Image
 from numpy._typing import NDArray
 
-from torchvision.transforms import functional as F, InterpolationMode
-
 from mnist_synthetic.generator import NumbersGenerator
 from mnist_synthetic.torch.datasets import MNISTSynthetic
+from mnist_synthetic.utils.rotation import Rotation
 
 
 class MNISTSyntheticRotate(MNISTSynthetic):
     def __init__(
         self,
         size: int,
-        max_angle: int,
         root: str | Path | None = None,  # type: ignore[assignment]
         transforms: Optional[Callable] = None,
         transform: Optional[Callable] = None,
         target_transform: Optional[Callable] = None,
         seed: int | None = None,
-        seed_angle: float | Literal['same'] | None = 'same',
-        resample: int = cv2.INTER_NEAREST,
+        rotate_before_np: Rotation | None = None,
+        rotate_before_pill: Rotation | None = None,
+        rotate_after: Rotation | None = None,
     ):
-        self.max_angle = max_angle
-        self.resample: int = resample
+        assert sum(map(lambda v: v is not None, [rotate_before_np, rotate_before_pill, rotate_after])) == 1,\
+            "This is Rotate module, rotate transform is required"
+
+        self._rotate_before_np = rotate_before_np
+        self._rotate_before_pill = rotate_before_pill
+        self._rotate_after = rotate_after
+
         super().__init__(size, root, transforms, transform, target_transform, seed)
 
-        if seed_angle == 'same':
-            seed_angle = seed
+    def rotate_before_np(self, img: NDArray, angle: int | None = None) -> tuple[NDArray, int | None]:
+        if self._rotate_before_np is None:
+            return img, angle
 
-        self.random_angle = np.random.default_rng(seed=seed_angle)
+        return self._rotate_before_np(img, angle=angle)
+
+    def rotate_before_pill(self, img: Image.Image, angle: int  | None = None) -> tuple[Image.Image, int | None]:
+        if self._rotate_before_pill is None:
+            return img, angle
+
+        return self._rotate_before_pill(img, angle=angle)
+
+    def rotate_tensor(self, img: torch.Tensor, angle: int | None = None) -> tuple[torch.Tensor, int | None]:
+        if self._rotate_after is None:
+            return img, angle
+
+        return self._rotate_after(img, angle=angle)
+
+    @property
+    def rotate_transform(self) -> Rotation:
+        return self._rotate_before_np or self._rotate_before_pill or self._rotate_after
 
     def __getitem__(self, idx: int) -> tuple[Any, int, int]:
         img_np = self.data[idx]
         target: int = self.targets[idx]
 
+        img_np, angle = self.rotate_before_np(img_np)
+
         # Many transform methods required Pillow
         img = Image.fromarray(img_np, mode="L")
+        img, angle = self.rotate_before_pill(img, angle=angle)
         if self.transform is not None:
             img = self.transform(img)
 
-        angle = 0
-        if self.max_angle > 0:
-            angle = int(self.random_angle.integers(-self.max_angle, self.max_angle))
-            img = F.rotate(img, angle, interpolation=InterpolationMode.BILINEAR)
-            # img = rotation.rotate_image_np(img, angle, mode=self.resample)
-            angle = -angle
+        img, angle = self.rotate_tensor(img, angle=angle)
 
         if self.target_transform is not None:
             target = self.target_transform(target)
 
-        return img, target, angle
+        return img, target, -angle
 
 
 class MNISTSyntheticRotated(MNISTSyntheticRotate):
@@ -62,21 +81,27 @@ class MNISTSyntheticRotated(MNISTSyntheticRotate):
     def _generate_image(self, generator: NumbersGenerator) -> tuple[NDArray[np.uint8], int, dict[str, Any] | None]:
         img, label = generator.generate()
 
-        # img = ToCenterNumpy(offset=None, resample=self.resample)(img)
+        img, angle = self.rotate_before_np(img)
+        if angle is None:
+            angle = self.rotate_transform.generate_angle()
 
-        angle = 0
-        if self.max_angle > 0:
-            # angle = generator.seed_rng.integers(-self.max_angle, self.max_angle)
-            # img = rotation.rotate_image_np(img, angle, mode=self.resample)
-            angle = -angle
-
+        assert angle is not None, 'Angle must be generated'
         return img, int(label), {'angle': angle}
 
-    def __getitem__(self, idx: int) -> tuple[Any, int, int]:
-        img, target = super(MNISTSyntheticRotate, self).__getitem__(idx)
+    def __getitem__(self, idx: int) -> tuple[Any, Any, int]:
+        img_np: NDArray[np.uint8] = self.data[idx]
+        target: int = self.targets[idx]
 
-        angle = self.extra[idx]['angle']
-        if abs(angle) > 0:
-            img = F.rotate(img, -angle, interpolation=InterpolationMode.BILINEAR)
+        # Many transform methods required Pillow
+        img = Image.fromarray(img_np, mode="L")
+        img, angle = self.rotate_before_pill(img, angle=self.extra[idx]['angle'])
 
-        return img, target, self.extra[idx]['angle']
+        if self.transform is not None:
+            img = self.transform(img)
+
+        img, angle = self.rotate_tensor(img, angle=angle)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target, -angle
